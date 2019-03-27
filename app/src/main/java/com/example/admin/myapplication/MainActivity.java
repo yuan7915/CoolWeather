cc18c593ae1d8d9d8b547675d800aae10269e6b6
admin <1656782633@qq.com>
2019-03-27 15:40:16 +0800

    作业

--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.admin.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.textView=(TextView) findViewById(R.id.abc);
+
+        String weatherId="CN101010200";
+        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=284c0b03c7a247dd8fb171e45f2e0a1e";
+        HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseText = response.body().string();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(responseText);
+                    }
+                });
+            }
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+        });
     }
 }
