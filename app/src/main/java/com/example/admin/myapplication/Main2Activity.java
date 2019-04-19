7f0ddc88db375a0bf786361971ce59593f44f1ae
admin <1656782633@qq.com>
2019-04-19 09:16:25 +0800

    作业

--- app/src/main/java/com/example/admin/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/admin/myapplication/Main2Activity.java
@@ -3,7 +3,9 @@ package com.example.admin.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -14,13 +16,17 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
 public class Main2Activity extends AppCompatActivity {
-    private String[] data = {"北京", "浙江", "安徽","","","","","","","","","","","","","","","","","","","",""};
+    private List<String> data2=new ArrayList();
+    private int[] pids=new int[]{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,};
+    private String[] data = {"北京", "浙江", "安徽","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""};
     private TextView textView;
     private Button button;
     private ListView listview;
@@ -31,15 +37,25 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         this.textView = (TextView) findViewById(R.id.textView);
         this.listview = (ListView) findViewById(R.id.listview);
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
         listview.setAdapter(adapter);
+        this.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick( AdapterView<?> parent, View view, int position, long id ) {
+                Log.i("点击了哪一个",""+position+":"+Main2Activity.this.pids[position]+":"+Main2Activity.this);
+                Intent intent=new Intent(Main2Activity.this,MainActivity.class);
+                intent.putExtra("pid",Main2Activity.this.pids[position]);
+                startActivity(intent);
+            }
+        });
 
         String weatherUrl = "http://guolin.tech/api/china";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback() {
             @Override
             public void onResponse( Call call, Response response ) throws IOException {
                 final String responseText = response.body().string();
-                String[] rosult = parseJSONObject(responseText);
+                String[] result = parseJSONObject(responseText);
                 Main2Activity.this.data=result;
                 runOnUiThread(new Runnable() {
                     @Override
@@ -63,8 +79,10 @@ public class Main2Activity extends AppCompatActivity {
             String[] result = new String[jsonArray.length()];
             for (int i = 0; i<jsonArray.length(); i++) {
                 JSONObject jsonObject=null;
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                jsonObject = jsonArray.getJSONObject(i);
                 result[i] = jsonObject.getString("name");
+                this.data[i]=jsonObject.getString("name");
+                this.pids[i]=jsonObject.getInt("id");
             }
             return result;
         } catch (JSONException e) {
--- app/src/main/java/com/example/admin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/admin/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.admin.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent=getIntent();
+        int pid=intent.getIntExtra("pid",0);
+        Log.i("我们接收到了id",""+pid);
         this.textView=(TextView) findViewById(R.id.abc);
         this.button=(Button)findViewById(R.id.btn);
         this.button.setOnClickListener(new View.OnClickListener(){
